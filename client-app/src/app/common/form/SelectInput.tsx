73eaee3187b6f0ee4bed28a0acd7b1deb9ac07af
luprok <luprok@email.com>
2020-10-19 16:38:24 +0200

    End of section 11

---
+++ client-app/src/app/common/form/SelectInput.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FieldRenderProps } from 'react-final-form'
+import { Form, FormFieldProps, Label, Select } from 'semantic-ui-react'
+
+interface IProps extends FieldRenderProps<string, HTMLElement>, FormFieldProps {}
+
+const SelectInput: React.FC<IProps> = ({input, width, options, placeholder, meta: {touched, error}}) => {
+    return (
+        <Form.Field error={touched &&  !!error}  width={width}>
+            <Select
+            value={input.value}
+            onChange={(e, data)=> input.onChange(data.value)}
+            placeholder={placeholder}
+            options={options}
+            />
+            {touched && error &&(
+                <Label baseic color='red'>{error}</Label>
+            )}
+        </Form.Field>
+    )
+}
+
+export default SelectInput
--- client-app/src/app/stores/ActivityStore.ts
+++ client-app/src/app/stores/ActivityStore.ts
@@ -1,5 +1,7 @@
 import { action, observable, computed, configure, runInAction } from "mobx";
 import { createContext, SyntheticEvent } from "react";
+import { toast } from "react-toastify";
+import { history } from "../..";
 import agent from "../api/agent";
 import { IActivity } from "../models/activity";
 
@@ -26,12 +28,12 @@ class ActivityStore {
 
   groupActivitiesByDate(activities: IActivity[]) {
     const sortedActivities = activities.sort(
-      (a, b) => Date.parse(a.date) - Date.parse(b.date)
+      (a, b) => a.date.getTime() - b.date.getTime()
     );
 
     return Object.entries(
       sortedActivities.reduce((activities, activity) => {
-        const date = activity.date.split("T")[0];
+        const date = activity.date.toISOString().split("T")[0];
         activities[date] = activities[date]
           ? [...activities[date], activity]
           : [activity];
@@ -47,7 +49,7 @@ class ActivityStore {
       const activities = await agent.Activities.list();
       runInAction("loading activities", () => {
         activities.forEach((activity) => {
-          activity.date = activity.date.split(".")[0];
+          activity.date = new Date(activity.date);
           this.activityRegistry.set(activity.id, activity);
         });
         this.loadingInitial = false;
@@ -65,14 +67,18 @@ class ActivityStore {
     let activity = this.getActivity(id);
     if (activity) {
       this.activity = activity;
+      return activity;
     } else {
       this.loadingInitial = true;
       try {
         activity = await agent.Activities.details(id);
         runInAction("getting activity", () => {
+          activity.date = new Date(activity.date);
           this.activity = activity;
+          this.activityRegistry.set(activity.id, activity);
           this.loadingInitial = false;
         });
+        return activity;
       } catch (error) {
         runInAction("getting activity error", () => {
           this.loadingInitial = false;
@@ -98,11 +104,13 @@ class ActivityStore {
         this.activityRegistry.set(activity.id, activity);
         this.submitting = false;
       });
+      history.push(`/activities/${activity.id}`)
     } catch (error) {
       runInAction("create activity error", () => {
-        console.log(error);
         this.submitting = false;
       });
+      toast.error('Problem submitting data')
+      console.log(error.response);
     }
   };
 
@@ -115,11 +123,13 @@ class ActivityStore {
         this.activity = activity;
         this.submitting = false;
       });
+      history.push(`/activities/${activity.id}`)
     } catch (error) {
       runInAction("edit activity error", () => {
         this.submitting = false;
-        console.log(error);
       });
+      console.log(error.response);
+      toast.error('Problem submitting data')
     }
   };
 
